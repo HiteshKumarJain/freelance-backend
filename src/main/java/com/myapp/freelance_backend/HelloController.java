a4e433112ed4cb0cf9df39cb59e1ce85e6fd9c49
HiteshKumarJain <hiteshkumarjainhkj@gmail.com>
2025-12-17 12:22:48 +0530

    Day 1: First endpoint

---
+++ src/main/java/com/myapp/freelance_backend/HelloController.java
@@ -0,0 +1,13 @@
+package com.myapp.freelance_backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Day 1 Complete! My first API!";
+    }
+}
