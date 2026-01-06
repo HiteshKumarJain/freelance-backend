b74faec1606e4aa257c81e9004e5a83d3f59ac13
HiteshKumarJain <hiteshkumarjainhkj@gmail.com>
2026-01-06 21:40:29 +0530

    Day 3: Added greet endpoint with RequestParam

--- src/main/java/com/myapp/freelance_backend/HelloController.java
+++ src/main/java/com/myapp/freelance_backend/HelloController.java
@@ -1,6 +1,7 @@
 package com.myapp.freelance_backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Day 1 Complete! My first API!";
+    }
+
+    @GetMapping("/hello_name")
+    public String helloName(@RequestParam String name) {
+        return "Hello, " + name + " !";
     }
 
     @GetMapping("/goodbye")
