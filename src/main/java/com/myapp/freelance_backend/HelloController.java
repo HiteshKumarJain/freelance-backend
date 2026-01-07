cc75ae61837d825da058d0612fdad43e26d62395
HiteshKumarJain <hiteshkumarjainhkj@gmail.com>
2026-01-07 14:05:15 +0530

    Day 3: Added welcome endpoint with PathVariable

--- src/main/java/com/myapp/freelance_backend/HelloController.java
+++ src/main/java/com/myapp/freelance_backend/HelloController.java
@@ -1,6 +1,7 @@
 package com.myapp.freelance_backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +13,21 @@ public class HelloController {
         return "Day 1 Complete! My first API!";
     }
 
+//    The below is the code for @RequestParam
     @GetMapping("/hello_name")
-    public String helloName(@RequestParam String name) {
-        return "Hello, " + name + " !";
+    public String helloName(@RequestParam String yourname) {
+        return "Hello, " + yourname + " !";
     }
 
+//    The below is the code for @PathVariable
+    @GetMapping("/welcome/{name}")
+    public String welcome(@PathVariable String name){
+        return "Welcome Home, " + name;
+    }
     @GetMapping("/goodbye")
     public String goodbye(){
         return "That's it for Today,GOOD BYE..!!!";
     }
+
+
 }
